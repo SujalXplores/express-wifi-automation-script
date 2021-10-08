a739dbe4ad0e5573e098e2827030f665c9eb21fa
SujalShah3234 <shahc9437@gmail.com>
2021-10-08 23:17:19 +0530

    simple script to do automated task in single click

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ const { By, Key, Builder } = require('selenium-webdriver');
 require('chromedriver');
 
 async function example() {
-  var searchString = 'Automation testing with Selenium';
+  // var searchString = 'Automation testing with Selenium';
 
   //To wait for browser to build and launch properly
-  let driver = await new Builder().forBrowser('chrome').build();
+  const driver = await new Builder().forBrowser('chrome').build();
 
   //To fetch http://google.com from the browser with our code.
-  await driver.get('http://google.com');
+  await driver.get(
+    'https://tikona-standalone-ccpl-xwf-internet-org.tikona.in.expresswifi.com/customer/app'
+  );
 
   //To send a search query by passing the value in searchString.
-  await driver.findElement(By.name('q')).sendKeys(searchString, Key.RETURN);
+  // console.log(await driver.findElement(By.js()));
+
+  await driver.sleep(7000);
+
+  await driver
+    .findElement(By.css('button.ButtonMedium.BlockButton.PrimaryButton'))
+    .click(); // .sendKeys(searchString, Key.RETURN);
 
   //Verify the page title and print it
-  var title = await driver.getTitle();
+  const title = await driver.getTitle();
   console.log('Title is:', title);
 
   //It is always a safe practice to quit the browser after execution
+  await driver.sleep(40000);
+
   await driver.quit();
 }
 
