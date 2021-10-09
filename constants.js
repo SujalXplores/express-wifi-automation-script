0a27409f964efab9deae799acc3c578ffa03fdf1
SujalShah3234 <shahc9437@gmail.com>
2021-10-09 13:54:43 +0530

    ♿️ added constants in project for better managable code

---
+++ constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  URL_HOME:
+    'https://tikona-standalone-ccpl-xwf-internet-org.tikona.in.expresswifi.com/customer/app',
+  WATCH_AD_BUTTON_SELECTOR: 'button.ButtonMedium.BlockButton.PrimaryButton',
+};
