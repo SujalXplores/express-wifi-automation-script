7262a973df5f2cd85a33aa8741bd255271684739
SujalShah3234 <shahc9437@gmail.com>
2021-10-10 18:03:54 +0530

    minor imporvements in script

--- app.js
+++ app.js
@@ -2,44 +2,48 @@ const { By, Builder, until } = require('selenium-webdriver');
 const { URL_HOME, WATCH_AD_BUTTON_SELECTOR } = require('./constants');
 require('chromedriver');
 
-async function exWifi() {
-  //To wait for browser to build and launch properly
-  const driver = await new Builder().forBrowser('chrome').build();
-  try {
-    // Enters the given URL
-    await driver.get(URL_HOME);
+(() => {
+  'use strict';
 
-    // Maximize window and wait for button to load
-    // TODO: Find a way to wait until the button loads properly in the screen.
-    await driver.manage().window().maximize();
-    await driver.sleep(4000);
+  async function exWifi() {
+    //To wait for browser to build and launch properly
+    const driver = await new Builder().forBrowser('chrome').build();
+    try {
+      // Enters the given URL
+      await driver.get(URL_HOME);
 
-    // Find and click on the button by cssSelector
-    await driver.findElement(By.css(WATCH_AD_BUTTON_SELECTOR)).click();
+      // Maximize window and wait for button to load
+      // TODO: Find a way to wait until the button loads properly in the DOM.
+      await driver.manage().window().maximize();
+      await driver.sleep(4000);
 
-    // Waiting for ads to finish
-    await driver.sleep(2000);
-    await driver.wait(until.urlIs(URL_HOME));
-    await driver.sleep(2000);
+      // Find and click on the button by cssSelector
+      await driver.findElement(By.css(WATCH_AD_BUTTON_SELECTOR)).click();
 
-    //Verify the page title and print it
-    const title = await driver.getTitle();
-    const url = await driver.getCurrentUrl();
+      // Waiting for ads to finish
+      await driver.sleep(2000);
+      await driver.wait(until.urlIs(URL_HOME));
+      await driver.sleep(2000);
 
-    console.log('----------------------------------------------------');
-    console.log('📝 App name:', title);
-    console.log('🚨 URL:', url);
-    console.log('✅ Data successfully credited to your account.');
-    console.log('----------------------------------------------------');
+      //Verify the page title and print it
+      const title = await driver.getTitle();
+      const url = await driver.getCurrentUrl();
 
-    //It is always a safe practice to quit the browser after execution
-    await driver.close();
-    console.log('Window closed!');
-  } catch (e) {
-    console.log('🥅 Error Occurred!!!', e.message);
-    await driver.close();
-    return;
+      console.log('----------------------------------------------------');
+      console.log('📝 App name:', title);
+      console.log('🚨 URL:', url);
+      console.log('✅ Data successfully credited to your account.');
+      console.log('----------------------------------------------------');
+
+      //It is always a safe practice to quit the browser after execution
+      await driver.close();
+      console.log('Window closed!');
+    } catch (e) {
+      console.log('🥅 Error Occurred!!!', e.message);
+      await driver.close();
+      return;
+    }
   }
-}
 
-Promise.all([exWifi(), exWifi(), exWifi(), exWifi()]);
+  Promise.all([exWifi(), exWifi(), exWifi(), exWifi()]);
+})();
