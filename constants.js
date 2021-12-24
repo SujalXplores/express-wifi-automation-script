c75fd19e5ddb5527e9255e4d6f701499a8ebcd36
SujalShah3234 <shahc9437@gmail.com>
2021-12-24 13:30:10 +0530

    🐛 Fix: show ads on first load

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 module.exports = {
   URL_HOME:
-    'https://tikona-standalone-ccpl-xwf-internet-org.tikona.in.expresswifi.com/customer/app',
+    'https://tikona-standalone-ccpl-xwf-internet-org.tikona.in.expresswifi.com/customer/app#',
 };
